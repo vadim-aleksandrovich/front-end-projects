285bdf6aa90403b77259ab5d3657a56b06f025bb
Vadim Aleksandrovich <v.alexandrovich93@gmail.com>
2020-10-24 13:47:54 +0300

    add: background changer

--- momentum/script.js
+++ momentum/script.js
@@ -4,9 +4,31 @@ const date = document.querySelector('.date');
 const greeting = document.querySelector('.greeting');
 const name = document.querySelector('.name');
 const focus = document.querySelector('.focus');
-
+const buttonBg = document.querySelector('.button__bg');
 const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
 const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+
+let now = new Date;
+let counterBg = now.getHours();
+let timeFlag = now.getHours();
+let bgNight = ["url(assets/images/night/01.jpg)", "url(assets/images/night/02.jpg)", "url(assets/images/night/02.jpg)", "url(assets/images/night/03.jpg)", "url(assets/images/night/04.jpg)", "url(assets/images/night/05.jpg)", "url(assets/images/night/06.jpg)", "url(assets/images/night/07.jpg)", "url(assets/images/night/08.jpg)", "url(assets/images/night/09.jpg)", "url(assets/images/night/10.jpg)", "url(assets/images/night/11.jpg)", "url(assets/images/night/12.jpg)", "url(assets/images/night/13.jpg)", "url(assets/images/night/14.jpg)", "url(assets/images/night/15.jpg)" ];
+let bgMorning = ["url(assets/images/morning/01.jpg)", "url(assets/images/morning/02.jpg)", "url(assets/images/morning/02.jpg)", "url(assets/images/morning/03.jpg)", "url(assets/images/morning/04.jpg)", "url(assets/images/morning/05.jpg)", "url(assets/images/morning/06.jpg)", "url(assets/images/morning/07.jpg)", "url(assets/images/morning/08.jpg)", "url(assets/images/morning/09.jpg)", "url(assets/images/morning/10.jpg)", "url(assets/images/morning/11.jpg)", "url(assets/images/morning/12.jpg)", "url(assets/images/morning/13.jpg)", "url(assets/images/morning/14.jpg)", "url(assets/images/morning/15.jpg)"];
+let bgAfternoon = ["url(assets/images/day/01.jpg)", "url(assets/images/day/02.jpg)", "url(assets/images/day/02.jpg)", "url(assets/images/day/03.jpg)", "url(assets/images/day/04.jpg)", "url(assets/images/day/05.jpg)", "url(assets/images/day/06.jpg)", "url(assets/images/day/07.jpg)", "url(assets/images/day/08.jpg)", "url(assets/images/day/09.jpg)", "url(assets/images/day/10.jpg)", "url(assets/images/day/11.jpg)", "url(assets/images/day/12.jpg)", "url(assets/images/day/13.jpg)", "url(assets/images/day/14.jpg)", "url(assets/images/day/15.jpg)"];
+let bgEvening = ["url(assets/images/evening/01.jpg)", "url(assets/images/evening/02.jpg)", "url(assets/images/evening/02.jpg)", "url(assets/images/evening/03.jpg)", "url(assets/images/evening/04.jpg)", "url(assets/images/evening/05.jpg)", "url(assets/images/evening/06.jpg)", "url(assets/images/evening/07.jpg)", "url(assets/images/evening/08.jpg)", "url(assets/images/evening/09.jpg)", "url(assets/images/evening/10.jpg)", "url(assets/images/evening/11.jpg)", "url(assets/images/evening/12.jpg)", "url(assets/images/evening/13.jpg)", "url(assets/images/evening/14.jpg)", "url(assets/images/day/15.jpg)"];
+
+let partOfDay;
+let bgNightSort = bgNight.sort(() => Math.random() - 0.5);
+let bgMorningSort = bgMorning.sort(() => Math.random() - 0.5);
+let bgAfternoonSort = bgAfternoon.sort(() => Math.random() - 0.5);
+let bgEveningSort = bgEvening.sort(() => Math.random() - 0.5);
+
+let backgroundArray = [];
+for (let i = 0; i < 6; i++) backgroundArray.push(bgNightSort[i]);
+for (let i = 0; i < 6; i++) backgroundArray.push(bgMorningSort[i]);
+for (let i = 0; i < 6; i++) backgroundArray.push(bgAfternoonSort[i]);
+for (let i = 0; i < 6; i++) backgroundArray.push(bgEveningSort[i]);
+//(new Image()).src = url1;
+
 
 // Show Time and Date
 showTime = () => {
@@ -31,27 +53,27 @@ date.innerHTML = `${getWeekDay(day)}, ${dayNumber} ${getMonth(month)}`;
 setTimeout(showTime, 1000); //1000
 };
 
-//? Set Background and Greeting
+// //? Set Background and Greeting
+let setBg = () => {
+    let today = new Date();
+    let hour = today.getHours();
+    document.body.style.backgroundImage = backgroundArray[hour];
+}
+
 setBgGreet = () => {
     let today = new Date();
     let hour = today.getHours();
-
     if (hour >= 6 && hour < 12) {
-        document.body.style.backgroundImage = "url('../momentum/assets/images/morning/01.jpg')";
         greeting.textContent = 'Good Morning';
     }
     if (hour >= 12 && hour < 18) {
-        document.body.style.backgroundImage = "url('../momentum/assets/images/day/01.jpg')";
         greeting.textContent = 'Good Afternoon';
     }
     if (hour >= 18 && hour < 24) {
-        document.body.style.backgroundImage = "url('../momentum/assets/images/evening/01.jpg')";
         greeting.textContent = 'Good Evening';
     }
     if (hour >= 0 && hour < 6) {
-        document.body.style.backgroundImage = "url('../momentum/assets/images/evening/01.jpg')";
         greeting.textContent = 'Good Night';
-        document.body.style.color = 'white';
     }
 }
 
@@ -109,8 +131,17 @@ focus.addEventListener ('click', function () {focus.textContent = '';});
 focus.addEventListener('keypress', setFocus);
 focus.addEventListener('blur', setFocus);
 
+buttonBg.addEventListener('click', function () {
+    counterBg < 23 ? counterBg +=1 : counterBg = timeFlag;
+    document.body.style.backgroundImage = backgroundArray[counterBg];
+    console.log(`image: ${backgroundArray[counterBg]}`);
+    console.log(`counter : ${counterBg}`)
+});
+
+
 //? Run
 showTime();
+setBg();
 setBgGreet();
 getName();
 getFocus();
