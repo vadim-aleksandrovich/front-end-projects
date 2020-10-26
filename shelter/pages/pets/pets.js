001c8a08e5bf7670a3a9a420eb4c27759ad4d6fd
Vadim Aleksandrovich <v.alexandrovich93@gmail.com>
2020-10-26 23:10:52 +0300

    refact: refactor burger menu for pets page

--- shelter/pages/pets/pets.js
+++ shelter/pages/pets/pets.js
@@ -262,26 +262,30 @@ windowModal.addEventListener("click", function (e) {
 // ! BURGER FUNCTION
 
 const burgerBtn = document.querySelector(".menu_burger_link");
-const burgerWindow = document.querySelector(".burger__window");
+const burgerWindow = document.querySelector(".burger__overlay");
+const menu = document.querySelector(".menu");
 const burgerLinkActive = document.querySelector(".burger__link_active");
 const body = document.querySelector("body");
 
 burgerBtn.addEventListener("click", function () {
   burgerBtn.classList.toggle("menu_burger_link_active");
-  burgerWindow.classList.toggle("burger__window_active");
+  burgerWindow.classList.toggle("burger__overlay_active");
+  menu.classList.toggle("menu_active");
   body.classList.toggle("overflow-hidden");
 });
 
 burgerWindow.addEventListener("click", function (e) {
   if (!e.target.closest(".burger__box")) {
-    burgerWindow.classList.toggle("burger__window_active");
     burgerBtn.classList.toggle("menu_burger_link_active");
-    body.classList.toggle("overflow-hidden");
+  burgerWindow.classList.toggle("burger__overlay_active");
+  menu.classList.toggle("menu_active");
+  body.classList.toggle("overflow-hidden");
   }
 });
 
 burgerLinkActive.addEventListener("click", function () {
-  burgerWindow.classList.remove("burger__window_active");
-  burgerBtn.classList.remove("menu_burger_link_active");
+  burgerBtn.classList.toggle("menu_burger_link_active");
+  burgerWindow.classList.toggle("burger__overlay_active");
+  menu.classList.toggle("menu_active");
   body.classList.toggle("overflow-hidden");
 });
