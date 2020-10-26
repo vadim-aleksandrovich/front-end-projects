ccd6247ddb29fb210679bd3d775b0645f2cb7fea
Vadim Aleksandrovich <v.alexandrovich93@gmail.com>
2020-10-27 00:08:03 +0300

    refact: refactor pets js

--- shelter/pages/pets/pets.js
+++ shelter/pages/pets/pets.js
@@ -167,17 +167,101 @@ const popupCreator = (i) => {
     "<strong>Parasites: </strong>" + petsArr[i].parasites;
 };
 
-let cardIndex = [];
-for (let index = 0; index < 8; index++) {
-  cardIndex.push(index);
-}
+// let cardIndex = [];
+// for (let index = 0; index < 8; index++) {
+//   cardIndex.push(index);
+// }
 
-let cardAdder = () => {
-  for (let j = 0; j < 6; j++) {
-    cardIndex.sort((a, b) => Math.random() * 2 - 1);
-    cardIndex.forEach((i) => cardCreator(i));
+// let cardAdder = () => {
+//   for (let j = 0; j < 6; j++) {
+//     cardIndex.sort((a, b) => Math.random() * 2 - 1);
+//     cardIndex.forEach((i) => cardCreator(i));
+//   }
+// };
+
+// *NEW RANDOM & SORT FUNCTION
+let cardIndex = [], // *48
+petsIndex = []; // *8
+
+// *NEW
+for (let index = 0; index < 8; index++) petsIndex.push(index);
+
+cardIndex = (() => {
+    let tempArr = [];
+
+    for (let i = 0; i < 6; i++) {
+      const newPets = petsIndex;
+
+      for (let j = petsIndex.length; j > 0; j--) {
+        let randInd = Math.floor(Math.random() * j);
+        const randElem = newPets.splice(randInd, 1)[0];
+        newPets.push(randElem);
+      }
+
+      tempArr = [...tempArr, ...newPets];
+    }
+    return tempArr;
+  })();
+
+  const sort863 = (list) => {
+    let unique8List = [];
+    let length = list.length;
+    for (let i = 0; i < length / 8; i++) {
+      const uniqueStepList = [];
+      for (j = 0; j < list.length; j++) {
+        if (uniqueStepList.length >= 8) {
+          break;
+        }
+        const isUnique = !uniqueStepList.some((item) => {
+          return item === list[j];
+        });
+        if (isUnique) {
+          uniqueStepList.push(list[j]);
+          list.splice(j, 1);
+          j--;
+        }
+      }
+      unique8List = [...unique8List, ...uniqueStepList];
+    }
+    list = unique8List;
+
+
+    list = sort6recursively(list);
+
+    return list;
   }
-};
+
+  const sort6recursively = (list) => {
+    const length = list.length;
+
+    for (let i = 0; i < (length / 6); i++) {
+      const stepList = list.slice(i * 6, (i * 6) + 6);
+
+      for (let j = 0; j < 6; j++) {
+        const duplicatedItem = stepList.find((item, ind) => {
+          return item === stepList[j] && (ind !== j);
+        });
+
+        if (duplicatedItem !== undefined) {
+          const ind = (i * 6) + j;
+          const which8OfList = Math.trunc(ind / 8);
+
+          list.splice(which8OfList * 8, 0, list.splice(ind, 1)[0]);
+
+          sort6recursively(list);
+        }
+      }
+    }
+
+    return list;
+  }
+
+  cardIndex = sort863(cardIndex);
+
+  let cardAdder = () => cardIndex.forEach((i) => cardCreator(i));
+  // *NEW
+
+
 
 cardAdder();
 
