ac88d7e61c7d1a5e2180e94d355ec15e05766572
Vadim Aleksandrovich <v.alexandrovich93@gmail.com>
2020-10-31 13:26:44 +0300

    feat: implemenment storage.js

---
+++ virtual-keyboard/js/storage.js
@@ -0,0 +1,8 @@
+export function set(name, value) {
+  window.localStorage.setItem(name, JSON.stringify());
+}
+
+export function get(name, subst = null) {
+  return JSON.parse(window.localStorage.getItem(name) || subst);
+}
+
