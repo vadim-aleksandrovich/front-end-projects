6c0b94f17c27aa2c70ae88bb7c27b9db2d337e64
Vadim Aleksandrovich <v.alexandrovich93@gmail.com>
2020-11-05 21:58:54 +0300

    feat: implement filed with gems

--- gem-puzzle/js/script.js
+++ gem-puzzle/js/script.js
@@ -2,3 +2,146 @@
 
 import create from './create.js';
 
+const menuOverlay = create('div', 'menu__overlay');
+const header = create('header', 'header');
+const main = create('main', 'main');
+const logo = create('div', 'logo', '', header);
+
+create('img', 'logo__img', '', logo, ['src', './assets/puzzle.svg']);
+create('h1', 'logo__title', 'Gem Puzzle', logo);
+create('audio', 'moveSound', create('source', '', '', '', ['src', './sound/move.mp3']), main, ['id', 'sound']);
+
+document.body.prepend(main);
+document.body.prepend(header);
+document.body.prepend(menuOverlay);
+
+const gameElements = create('div', 'game__elements', '', header);
+const timer = create('div', 'timmer', create('img', 'timer__icon', '', '', ['src', './assets/timer.png']), gameElements);
+const timerValue = create('span', 'timer__value', '00:00', timer);
+const steps = create('div', 'steps', create('img', 'steps__icon', '', '', ['src', './assets/steps.png']), gameElements);
+const stepsValue = create('span', 'steps__value', '0', steps);
+const burgerBtn = create('button', 'burger__btn', '<svg class="menu_burger_svg" width="30" height="22" viewBox="0 0 30 22" fill=""xmlns="http://www.w3.org/2000/svg"><line y1="21" x2="30" y2="21" stroke="#fafafa" stroke-width="2" /><line y1="11" x2="30" y2="11" stroke="#fafafa" stroke-width="2" /><line y1="1" x2="30" y2="1" stroke="#fafafa" stroke-width="2" /></svg>', header);
+const field = create('div', 'field', '', main);
+const menu = create('div', 'menu', '', main);
+
+const sizeSelector = create('input', '', '', menu,
+  ['type', 'range'],
+  ['id', 'sizeSelector'],
+  ['min', '3'],
+  ['max', '8'],
+  ['step', '1']);
+sizeSelector.value = 4;
+
+//Game's parametrs
+
+const windowSize = 600;
+let cellSize = windowSize / sizeSelector.value;
+let fieldSize = sizeSelector.value ** 2;
+
+let stepCounter = 0;
+let cells = [];
+let solution = [];
+
+let timerSeconds = 0;
+let timerMinutes = 0;
+let timerCount;
+
+const addZero = (n) => (n < 10 ? `0${n}` : n);
+
+let empty = {
+  value: fieldSize,
+  top: Math.sqrt(fieldSize) - 1,
+  left: Math.sqrt(fieldSize) - 1,
+};
+
+let cellEmpty = create('div', 'cell__empty', '', '');
+
+function emptyCellMove() {
+  cellEmpty.style.left = `${cells[fieldSize - 1].left * cellSize}rem`;
+  cellEmpty.style.top = `${cells[fieldSize - 1].top * cellSize}rem`;
+}
+
+function move(index, param) {
+  const cell = cells[index];
+  const leftDifferent = Math.abs(empty.left - cell.left);
+  const topDifferent = Math.abs(empty.top - cell.top);
+
+  if (param !== 'solution') {
+    if (leftDifferent + topDifferent > 1) {
+      return;
+    }
+    stepCounter += 1;
+    solution.unshift(index);
+  }
+
+  stepsValue.textContent = stepCounter;
+
+  cell.element.style.left = `${empty.left * cellSize}rem`;
+  cell.element.style.top = `${empty.top * cellSize}rem`;
+  const emptyLeft = empty.left;
+  const emptyTop = empty.top;
+  empty.left = cell.left;
+  cellEmpty.left = cell.left;
+  empty.top = cell.top;
+  cellEmpty.top = cell.top;
+  cell.left = emptyLeft;
+  cell.top = emptyTop;
+  emptyCellMove();
+}
+
+function restartGame() {
+  while (field.children.length) {
+    field.removeChild(field.lastChild);
+  }
+  cellSize = windowSize / sizeSelector.value;
+  fieldSize = sizeSelector.value ** 2;
+  cells = [];
+  solution = [];
+  empty.value = fieldSize;
+  empty.top = Math.sqrt(fieldSize) - 1;
+  empty.left = Math.sqrt(fieldSize) - 1;
+  field.style.width = `${Math.sqrt(fieldSize) * cellSize}rem`;
+  field.style.height = `${Math.sqrt(fieldSize) * cellSize}rem`;
+  startGame(fieldSize);
+  stepCounter = 0;
+  stepsValue.textContent = stepCounter;
+  clearInterval(timerCount);
+  timerSeconds = 0;
+  timerMinutes = 0;
+  timerCount = setInterval(gameTimer, 1000);
+  timerValue.textContent = `${addZero(timerMinutes)}:${addZero(timerSeconds)}`;
+}
+
+function startGame(size) { // (size == fieldSize)
+  const numbers = [...Array(size - 1).keys()];
+  field.style.pointerEvents = '';
+  for (let i = 0; i < size - 1; i += 1) {
+    const cell = document.createElement('div');
+    const value = numbers[i] + 1;
+    cell.className = 'cell';
+    cell.innerHTML = value;
+
+    const left = i % Math.sqrt(size);
+    const top = (i - left) / Math.sqrt(size);
+
+    cells.push({
+      value,
+      left,
+      top,
+      element: cell,
+      posX: `${-(left * Math.sqrt(size) * cellSize) / Math.sqrt(size)}rem`,
+      posY: `${-(top * Math.sqrt(size) * cellSize) / Math.sqrt(size)}rem`,
+    });
+
+    cell.style.width = `${cellSize}rem`;
+    cell.style.height = `${cellSize}rem`;
+    cell.style.left = `${left * cellSize}rem`;
+    cell.style.top = `${top * cellSize}rem`;
+
+    field.append(cell);
+    cell.addEventListener('click', () => {
+      move(i);
+    });
+}
+}
+restartGame()
